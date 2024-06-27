557c914ea5c816dd49152092be36059eaa7380b1
Jordan Wallace-Addai <jordan-wallace-addai@jordans-mbp.home>
2024-06-26 22:46:54 -0400

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.querySelector('#item-form');
 const itemInput = document.querySelector('#item-input');
 const itemList = document.querySelector('#item-list');
+const clearBtn = document.querySelector('#clear');
+const itemFilter = document.querySelector('#filter');
 
 const addItem = (e) => {
   e.preventDefault();
@@ -18,8 +20,11 @@ const addItem = (e) => {
   const button = createButton('remove-item btn-link text-red');
 
   li.appendChild(button);
-  // Add li to list
+  // Add li to list (DOM)
   itemList.appendChild(li);
+  // Check to see if items can be shown
+  checkUI();
+
   // Clear value after submission
   itemInput.value = '';
 };
@@ -40,5 +45,40 @@ const createIcon = (classes) => {
   return icon;
 };
 
+// Remove Item with event delegation
+const removeItem = (e) => {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      // Check to see if items can be shown
+      checkUI();
+    }
+  }
+};
+
+// Remove all list items
+const removeItems = () => {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  // Check to see if items can be shown
+  checkUI();
+};
+
+// Check and show elements depending on item length
+const checkUI = () => {
+  const items = itemList.querySelectorAll('li');
+
+  if (items.length === 0) {
+    itemFilter.style.display = 'none';
+    clearBtn.style.display = 'none';
+  } else {
+    itemFilter.style.display = 'block';
+    clearBtn.style.display = 'block';
+  }
+};
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', removeItems);
